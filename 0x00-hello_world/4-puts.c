8da9ed57793e8a34178b29c32c6ebb762530ee49
ELMEHDI <elferdielmehdi@gmail.com>
2023-12-14 06:46:56 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,18 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a new line"
- * return 0.
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a new line"
+ * Return: Always 0.
  */
 
+
 int main(void)
+
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	
 	return(0);
+
 }
